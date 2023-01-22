224bbe759e6529bb007314ba0029296763a4f1ea
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2023-01-22 08:01:23 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import Nasledjivanje.SubKlasaNastavnik;
 import Nasledjivanje.SuperKlasaOsoba;
 
 public class Main {
     public static void main(String[] args) {
 
-
+        SubKlasaNastavnik subKlasaNastavnik=new SubKlasaNastavnik();
+        subKlasaNastavnik.setIme("Kosta");
+        System.out.println("Ime nastavnika je : " + subKlasaNastavnik.getIme());
     }
 
 }
--- src/Nasledjivanje/SubKlasaNastavnik.java
+++ src/Nasledjivanje/SubKlasaNastavnik.java
@@ -1,9 +1,23 @@
 package Nasledjivanje;
 
-public class SubKlasaNastavnik {
+public class SubKlasaNastavnik extends SuperKlasaOsoba {
 
     private Double plata;
     private String predmet;
 
+    public Double getPlata() {
+        return plata;
+    }
 
+    public void setPlata(Double plata) {
+        this.plata = plata;
+    }
+
+    public String getPredmet() {
+        return predmet;
+    }
+
+    public void setPredmet(String predmet) {
+        this.predmet = predmet;
+    }
 }

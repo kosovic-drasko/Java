44eacfb0618f5144da10d018374b03d3ffa33e24
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2023-01-22 08:22:12 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import Nasledjivanje.SubKlasaNastavnik;
+import Nasledjivanje.SubKlasaUcenik;
 import Nasledjivanje.SuperKlasaOsoba;
 
 public class Main {
     public static void main(String[] args) {
 
         SubKlasaNastavnik subKlasaNastavnik=new SubKlasaNastavnik();
+        SubKlasaUcenik subKlasaUcenik=new SubKlasaUcenik();
         subKlasaNastavnik.setIme("Kosta");
         System.out.println("Ime nastavnika je : " + subKlasaNastavnik.getIme());
+        System.out.println("Svi podaci su : " + subKlasaNastavnik.getIme()+"\n"+subKlasaUcenik.toString());
+        subKlasaUcenik.setOcjene(5);
+        subKlasaUcenik.setRazred(9);
+        System.out.println("Ucenik podaci : " +subKlasaUcenik.toString());
+
     }
 
 }
--- src/Nasledjivanje/SubKlasaUcenik.java
+++ src/Nasledjivanje/SubKlasaUcenik.java
@@ -2,9 +2,52 @@ package Nasledjivanje;
 
 public class SubKlasaUcenik {
 
-    private String razred;
-    private  Double ocjene;
+    private Integer razred;
+    private  Integer ocjene;
 
-    public SubKlasaUcenik() {
+    public Integer getRazred() {
+        return razred;
     }
+
+    public void setRazred(Integer razred) {
+        this.razred = razred;
+    }
+
+    public Integer getOcjene() {
+        return ocjene;
+    }
+
+    public void setOcjene(Integer ocjene) {
+        this.ocjene = ocjene;
+    }
+
+    @Override
+    public String toString() {
+        return "SubKlasaUcenik{" +
+                "razred=" + razred +
+                ", ocjene=" + ocjene +
+                '}';
+    }
+    //    public SubKlasaUcenik(Integer razred, Integer ocjene) {
+//        this.razred = razred;
+//        this.ocjene = ocjene;
+//    }
+
+    // Sa konstuktorom punim
+
+//    public Integer getRazred() {
+//        return razred;
+//    }
+//
+//    public void setRazred(Integer razred) {
+//        this.razred = razred;
+//    }
+//
+//    public Integer getOcjene() {
+//        return ocjene;
+//    }
+//
+//    public void setOcjene(Integer ocjene) {
+//        this.ocjene = ocjene;
+//    }
 }

4f49a4b361d1d810c942cd3647f01f034f8040b8
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2023-02-04 09:50:09 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,34 @@
-
+import Interfejsi.ISkola;
 import Interfejsi.Skola;
 import Nasledjivanje.SubKlasaNastavnik;
 import Nasledjivanje.SubKlasaUcenik;
-import abstrakna.Pig;
+import Nasledjivanje.SuperKlasaOsoba;
+import Static.AppLog;
 
+import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        SubKlasaNastavnik subKlasaNastavnik=new SubKlasaNastavnik();
-        SubKlasaUcenik subKlasaUcenik=new SubKlasaUcenik(8,15);
-        subKlasaNastavnik.setIme("Kosta");
-        System.out.println("Ime nastavnika je : " + subKlasaNastavnik.getIme());
-        System.out.println("Svi podaci su : " + subKlasaNastavnik.getIme()+"\n"+subKlasaUcenik.toString());
-        subKlasaUcenik.setOcjene(5);
-        subKlasaUcenik.setRazred(9);
-        System.out.println("Ucenik podaci : " +subKlasaUcenik.toString());
+//        SubKlasaNastavnik subKlasaNastavnik=new SubKlasaNastavnik();
+//        SubKlasaUcenik subKlasaUcenik=new SubKlasaUcenik();
+//        subKlasaNastavnik.setIme("Kosta");
+//        System.out.println("Ime nastavnika je : " + subKlasaNastavnik.getIme());
+//        System.out.println("Svi podaci su : " + subKlasaNastavnik.getIme()+"\n"+subKlasaUcenik.toString());
+//        subKlasaUcenik.setOcjene(5);
+//        subKlasaUcenik.setRazred(9);
+//        System.out.println("Ucenik podaci : " +subKlasaUcenik.toString());
+//
+//
+//        //INTEFREJSI
+//        Skola skola=new Skola();
+//        System.out.println(skola.print());
 
 
+        AppLog al1 = new AppLog();
+        al1.write(3401,"Greska povezivanja");
 
-        //INTEFREJSI
-        Skola skola=new Skola();
-        System.out.println("Pisem iz inteface:   "+skola.print());
-        skola.kozola();
-
+        AppLog al2 = new AppLog();
+        al2.write(5000,"Greska 5000");
     }
-
-//Pig myPig =new Pig();
-//    myPig.
-//  https://www.w3schools.com/java/tryjava.asp?filename=demo_abstract
 }

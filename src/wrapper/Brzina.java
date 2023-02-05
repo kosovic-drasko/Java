8631ab2ee8d55bed5139cc68d0a3528fca5b8000
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2023-02-05 08:13:07 +0100

    init

---
+++ src/wrapper/Brzina.java
@@ -0,0 +1,10 @@
+package wrapper;
+
+public class Brzina {
+    private double mps;
+
+    private Brzina(double mps) {
+        this.mps = mps;
+    }
+
+}
--- src/wrapper/Put.java
+++ src/wrapper/Put.java
@@ -9,4 +9,21 @@ public class Put {
    public static Put fromMeters(double metar) {
        return new Put(metar);
    }
+    public static Put fromKilometars(double metar) {
+        return new Put(metar*1000);
+    }
+    public double toMeters(double metar){
+       return this.metar=metar;
+    }
+    public double toKilometers(double metar){
+        return this.metar=metar/1000;
+    }
+
+    public void addMeters(double metar){
+       this.metar+=metar;
+    }
+
+    public void addKilometers(double kilometri){
+        this.metar+=kilometri*1000;
+    }
 }

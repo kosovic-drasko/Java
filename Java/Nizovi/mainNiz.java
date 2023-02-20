28c5113c408b39a5ee9cbaa06e4a574c4d163d89
kosovic-drasko <kosovic.drasko@montefarmco.me>
2023-02-20 09:21:44 +0100

    init

---
+++ Java/Nizovi/mainNiz.java
@@ -0,0 +1,12 @@
+package Java.Nizovi;
+
+import java.util.Scanner;
+
+public class mainNiz {
+    public static void main(String[] args) {
+
+        Scanner s =new Scanner(System.in);
+        
+   
+    }
+}
--- Java/Petlje/MainPetlje.java
+++ Java/Petlje/MainPetlje.java
@@ -2,27 +2,43 @@ import java.util.Scanner;
 
 public class MainPetlje {
     public static void main(String[] args) {
-        int tajniBroj = 0;
-        int broj = 5;
-        int i;
-       
+      
+      
         try (Scanner s = new Scanner(System.in)) {
-            System.out.print("Unesite broj za petlju:  ");
-            broj = s.nextInt();
-            System.out.print("Unesite broj za tajni broj:  ");
-            tajniBroj = s.nextInt();
-            if (tajniBroj == 8) {
-                System.out.println("unjeli ste tajni broj  " + tajniBroj);
-    
-            }
-            else{
-                System.out.println("Broj koji ste unjeli nije tajni broj");
+            int tajniBroj = 54;
+            
+            for (int i = 0; i < 5; i++) {
+               
+               System.out.print("Unesite broj za petlju:  ");
+               int broj = s.nextInt();
+                // System.out.print("Unesite broj za tajni broj:  ");
+                // tajniBroj = s.nextInt();
+                if (broj == tajniBroj) {
+                    System.out.println("unjeli ste tajni broj  " + tajniBroj);
+                 
+                 break;
+                }
+                else{
+                    System.out.println(i);
+                    System.out.println("Broj koji ste unjeli nije tajni broj");
+                    
+                }
             }
         }
+        System.out.println("Program je zavrsen");
+        }
+       
 
-        for (i = 0; i < broj; i++) {
-            System.out.println(i);
-        }
+    //     Scanner s =new Scanner(System.in);
+    //     int izbor=-1;
+
+    //     while (izbor !=0){
+    //         System.out.print("Unesite broj:  ");
+    //         izbor=s.nextInt();
+    //         s.nextLine();
+    //     }
+    //     System.out.println("Kraj programa");
+    // }
 
     }
-}
+

14dd31cfe0adf71cff4152a6fee95bfcc9ff5b77
kosovic-drasko <kosovic.drasko@montefarmco.me>
2023-02-21 08:43:18 +0100

    init

--- Java/Nizovi/mainNiz.java
+++ Java/Nizovi/mainNiz.java
@@ -5,8 +5,23 @@ import java.util.Scanner;
 public class mainNiz {
     public static void main(String[] args) {
 
-        Scanner s =new Scanner(System.in);
-        
-   
+        try (Scanner s = new Scanner(System.in)) {
+            int niz[] = new int[3];
+
+            for (int i = 0; i < 3; i++) {
+                System.out.print("Unesite broj:  ");
+                niz[i] = s.nextInt();
+
+            }
+            double suma=0;
+            for (int i = 0; i < 3; i++) {
+                suma += niz[i];
+            }
+            double srednjaVrijednost=suma/3.0;
+            System.out.println("Ukupno je:  "+suma);
+            
+System.out.println("Prosjecna vrijednost je: "+srednjaVrijednost);
+        }
     }
+    
 }
